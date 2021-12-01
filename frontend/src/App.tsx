fa97a628d1dc96785d58a1885edfb9cb2addfcb1
nathanoliveiras <nathanoliveira98@gmail.com>
2021-12-01 19:15:40 -0300

    basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import Navbar from "components/Navbar";
+import Footer from "components/Footer"
+import DataTable from "components/DataTable"
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Hello world</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <h1 className="text-primary">Hello world</h1>
+        <DataTable/>
+      </div>
+      <Footer/>
+    </>
   );
 }
 
